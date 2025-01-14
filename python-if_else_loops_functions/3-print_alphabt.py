43052ac341559045e87268a861150a606c31e3be
root <root@DESKTOP-A00A4R8.>
2025-01-15 00:49:04 +0100

    fix task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(26):
+for i in range(97, 123):
     if i == 4 or i == 16:
         + i
     else:
-        print(chr(97 + i), end='')
+        print("{}".format(chr(i)), end='')
