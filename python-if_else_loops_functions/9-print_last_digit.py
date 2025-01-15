4588875be1b8a9e91a709bb014636f5853136abb
root <root@DESKTOP-A00A4R8.>
2025-01-15 01:31:48 +0100

    add task 9 to 12 done

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def print_last_digit(number):
+    if number < 0:
+        number = -number
+    last_nbr = number % 10
+    print("{}".format(last_nbr), end="")
+    return last_nbr
