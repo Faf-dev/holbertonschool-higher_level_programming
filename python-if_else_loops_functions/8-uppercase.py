f2ce89ec84bf467de9a803e15aa8cea4ceab691b
root <root@DESKTOP-A00A4R8.>
2025-01-15 01:49:28 +0100

    another attempt task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(str):
     result = ""
     for char in str:
-        if ord(char) >= 97 and ord(char) <= 122:
+        if 'a' <= char <= 'z':
             result += chr(ord(char) - 32)
         else:
             result += char
