e66f23b4a2b87ce0eb4598ed14d119a0df85dee9
root <root@DESKTOP-A00A4R8.>
2025-01-15 18:31:27 +0100

    push task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    argv_len = len(argv)
+    if argv_len == 1:
+        str = "arguments."
+    if argv_len == 2:
+        str = "argument:"
+    if argv_len > 2:
+        str = "arguments:"
+    print("{} {}".format(argv_len - 1, str))
+    for i in range(1, argv_len):
+        print("{}: {}".format(i, argv[i]))
