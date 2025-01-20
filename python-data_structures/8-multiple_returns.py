45983a61ef680c62290a1b6544b8be8a9eb6b929
root <root@DESKTOP-A00A4R8.>
2025-01-20 17:50:04 +0100

    push task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) < 1:
+        a = len(sentence)
+        b = None
+    else:
+        a = len(sentence)
+        b = sentence[0]
+    return a, b
