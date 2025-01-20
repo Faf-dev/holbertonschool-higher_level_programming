1a17a826e03c16f9f096f9108ee10c85b7354cde
root <root@DESKTOP-A00A4R8.>
2025-01-20 03:18:21 +0100

    push task 6

--- python-data_structures/6-main.py
+++ python-data_structures/6-main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+
+matrix = [
+    [16, 2, 3],
+    [4, 54, 6],
+    [76, 14, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in range(len(i)):
+            if i != 0:
+                print(" ", end="")
+            print("{}".format(i[j]), end="")
+        print()
