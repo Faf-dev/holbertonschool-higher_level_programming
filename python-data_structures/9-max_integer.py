e34934a0fd798bff576f6d6468c0ea2af0e04f03
root <root@DESKTOP-A00A4R8.>
2025-01-20 17:55:46 +0100

    push task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_int = my_list[0]
+    if len(my_list) < 1:
+        return None
+    for num in my_list:
+        if num > max_int:
+            max_int = num
+    return max_int
