29200162fea2152ac4439254bbdc8a4287c7a947
root <root@DESKTOP-A00A4R8.>
2025-01-20 02:41:01 +0100

    push task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    Cpy_list = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+        return Cpy_list
+    else:
+        Cpy_list[idx] = element
+        return Cpy_list
