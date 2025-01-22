78032dd3581154a1e45a9b0c1035a537ae19d66f
root <root@DESKTOP-A00A4R8.>
2025-01-22 01:23:04 +0100

    push task 6 to 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    else:
+        new_dictionary = sorted(a_dictionary, reverse=True)
+        for i in new_dictionary:
+            return i
