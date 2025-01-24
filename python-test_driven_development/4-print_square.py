cd704d4b720b2cfb612f7604bb65935989fdf382
Faf-dev <noaillesjulien@gmail.com>
2025-01-24 15:13:22 +0100

    push task 3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+print_square - print a square with #
+"""
+
+
+def print_square(size):
+    """
+    print_square :
+
+    parameters :
+    size : The size of the square
+
+    Return : Nothing
+    """
+    if not isinstance(size, int) or isinstance(size, float):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    
+    for i in range(size):
+        print("#" * size)
