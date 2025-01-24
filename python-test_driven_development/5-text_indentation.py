0c8b72ea1716144d5afce30fd1ee38f19ddc86ca
Faf-dev <noaillesjulien@gmail.com>
2025-01-24 16:23:24 +0100

    push task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+
+"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    print_text = ""
+    i = 0
+    while i < len(text):
+        print_text += text[i]
+        if text[i] in '.?:':
+            print_text += "\n\n"
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        i += 1
+
+    print(print_text.strip())
