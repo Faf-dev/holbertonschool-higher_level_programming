3cbd18f62ddf944f23046a2df958cbc0089728b1
Faf-dev <noaillesjulien@gmail.com>
2025-01-24 13:20:18 +0100

    fix pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,6 +2,8 @@
 """
 add_integer : add a with b
 """
+
+
 def add_integer(a, b=98):
     """
     add to integer
@@ -10,10 +12,10 @@ def add_integer(a, b=98):
     a : integer or float
     b : integer or float, 98 if only one argument
 
-    return : the addition of two int    
+    return : the addition of two int
     """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be an integer") 
+        raise TypeError("b must be an integer")
     return int(a) + int(b)
