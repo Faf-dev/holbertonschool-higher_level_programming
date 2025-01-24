1ab7b7f3b5fe32af340ec1cb4afd0cb792cb6f95
Faf-dev <noaillesjulien@gmail.com>
2025-01-24 13:04:30 +0100

    push task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+add_integer : add a with b
+"""
+def add_integer(a, b=98):
+    """
+    add to integer
+
+    args:
+    a : integer or float
+    b : integer or float, 98 if only one argument
+
+    return : the addition of two int    
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer") 
+    return int(a) + int(b)
