8a09050854b874c70fb49605efaa32e97d84ab4a
Faf-dev <noaillesjulien@gmail.com>
2025-01-27 17:43:34 +0100

    push task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""create a square class"""
+
+
+class Square:
+    """ define the square size"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
