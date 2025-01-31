fe552f62c611e97342d63c93c768aebb20cf740e
Faf-dev <noaillesjulien@gmail.com>
2025-01-31 12:34:43 +0100

    remove square to the end

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -30,10 +30,6 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
-    @classmethod
-    def square(cls, size=0):
-        return cls(height=size, width=size)
-
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
@@ -68,3 +64,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @classmethod
+    def square(cls, size=0):
+        return cls(height=size, width=size)
