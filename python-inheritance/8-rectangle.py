fd5d0d1b898925ce779158163d6c32899a72c331
Faf-dev <noaillesjulien@gmail.com>
2025-02-03 14:20:31 +0100

    push task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""Create an empty BaseGeometry class"""
+
+
+class BaseGeometry:
+    """define BaseGeometry class"""
+
+    def area(self):
+        """Raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value
+
+    Args:
+        name (str): name of the value
+        value (int): value to validate
+
+    Raises:
+        TypeError: If value is not an integer
+        ValueError: If value is less or equal to 0"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """define rectangle class"""
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
