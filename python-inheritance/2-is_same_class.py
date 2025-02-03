5f54a0dcb7cf3f06f8878f27332bd99bfd82031c
Faf-dev <noaillesjulien@gmail.com>
2025-02-03 12:39:47 +0100

    push task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Create a function who return True if
+obj is an instance of specified class"""
+
+
+def is_same_class(obj, a_class):
+    """is_same_class : check if obj is an instance of a_class
+
+    obj : instance of a_class
+    a_class : specified class
+
+    Return : True if obj is exactly an instance of a_class
+    false instead"""
+    return (type(obj) is a_class)
