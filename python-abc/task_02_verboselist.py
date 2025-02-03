b3d8dca124e19f46a8a601fb71f3d510602005c3
Faf-dev <noaillesjulien@gmail.com>
2025-02-03 22:18:08 +0100

    push task 2

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""create an ABC abstract class"""
+
+
+class VerboseList(list):
+    """Create abstract class Shape"""
+    def append(self, item):
+        """append method"""
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, iterable):
+        """extend method"""
+        super().extend(iterable)
+        print("Extended the list with [{}] items.".format(len(iterable)))
+
+    def remove(self, item):
+        """remove method"""
+        print("Removed [{}] from the list.".format(item))
+        super().remove(item)
+
+    def pop(self, index=-1):
+        print("Popped [{}] from the list.".format(self[index]))
+        super().pop(index)
