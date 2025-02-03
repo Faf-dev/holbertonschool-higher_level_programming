4bf3ae252fb5a7cb500694110a018b6b812a8dbf
Faf-dev <noaillesjulien@gmail.com>
2025-02-03 23:11:23 +0100

    push task 5

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""show that a Dragon instance can both swim and fly."""
+
+
+class SwimMixin:
+    """Mixin who create a swim method"""
+
+    def swim(self):
+        """swim method"""
+        print("The creature swims!")
+
+
+class FlyMixin:
+    """Mixin who create a flying method"""
+
+    def fly(self):
+        """fly method"""
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """Dragon class : inherits from swimmixin and flymixin"""
+
+    def roar(self):
+        """roar method"""
+        print("The dragon roars!")
