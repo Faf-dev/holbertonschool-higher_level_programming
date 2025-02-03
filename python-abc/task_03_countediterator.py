ed64954e26b71f17fd6dbe97890acc3fdc659287
Faf-dev <noaillesjulien@gmail.com>
2025-02-03 22:43:34 +0100

    push task 3

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""Extend the built in iter"""
+
+
+class CountedIterator:
+    """CountedIterator : extends the built-in iterator
+    obtained from the iter function
+    The CountedIterator should keep track of the number
+    of items that have been iterated over"""
+
+    def __init__(self, iterator):
+        """initialisation method"""
+        self.count = 0
+        self.iterator = iter(iterator)
+
+    def __next__(self):
+        """next method that increment count"""
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise StopIteration
+
+    def get_count(self):
+        """Return count of iterated items"""
+        return self.count
