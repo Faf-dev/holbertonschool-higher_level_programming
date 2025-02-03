4d4e2dd397a6bd9963b019b52eb7e562084ff09f
Faf-dev <noaillesjulien@gmail.com>
2025-02-03 11:54:18 +0100

    making all file executable + push task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Create MyList class"""
+
+
+class MyList(list):
+    """ define MyList class"""
+    def print_sorted(self):
+        sorted_list = sorted(self)
+        print(sorted_list)
