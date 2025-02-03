04bae4f59a9474f7056d13fa08f308e6d6a67919
Faf-dev <noaillesjulien@gmail.com>
2025-02-03 12:54:36 +0100

    push task 6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Create an empty BaseGeometry class"""
+
+
+class BaseGeometry:
+    """define BaseGeometry class"""
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
