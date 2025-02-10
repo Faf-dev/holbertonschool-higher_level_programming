7c1d93143a6f3d0504ac856e440cbc3c6b147543
Faf-dev <noaillesjulien@gmail.com>
2025-02-10 14:33:55 +0100

    fix task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     """method to open a file"""
-    with open(filename, encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data)
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read())
