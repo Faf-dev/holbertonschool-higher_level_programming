24aec568c5bf488ce3e7abbc4301093d778f1dd7
Faf-dev <noaillesjulien@gmail.com>
2025-02-11 23:18:32 +0100

    fix file not found error

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -11,16 +11,16 @@ def convert_csv_to_json(filename):
     filename : filename of the CSV file
     """
     data = []
+    try:
+        with open(filename, "r", encoding="utf-8") as csvf:
+            csvRead = csv.DictReader(csvf)
+            for rows in csvRead:
+                data.append(rows)
 
-    with open(filename, "r", encoding="utf-8") as csvf:
-        csvRead = csv.DictReader(csvf)
-        if csvRead is None:
-            return False
-        for rows in csvRead:
-            data.append(rows)
-
-    with open("data.json", "w", encoding="utf-8") as jsonf:
-        jsonf.write("[\n\t")
-        jsonf.write(",\n\t".join(json.dumps(row) for row in data))
-        jsonf.write("\n]")
-    return True
+        with open("data.json", "w", encoding="utf-8") as jsonf:
+            jsonf.write("[\n\t")
+            jsonf.write(",\n\t".join(json.dumps(row) for row in data))
+            jsonf.write("\n]")
+        return True
+    except FileNotFoundError:
+        return False
