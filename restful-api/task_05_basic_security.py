5c66f1c7287fe164b7359c36614bd5f87a4e71ba
Faf-dev <noaillesjulien@gmail.com>
2025-02-20 16:41:19 +0100

    fix pycodestyle

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -20,8 +20,18 @@ jwt = JWTManager(app)
 
 
 users = {
-    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
-    "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
+    "user1":
+    {
+        "username": "user1",
+        "password": generate_password_hash("password"),
+        "role": "user"
+        },
+    "admin1":
+    {
+        "username": "admin1",
+        "password": generate_password_hash("password"),
+        "role": "admin"
+        }
 }
 
 
