07ac51422a789b44260798298966efdd20c35a81
Faf-dev <noaillesjulien@gmail.com>
2025-03-03 14:33:54 +0100

    reduce from 4 lines to 1 line

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,2 @@
 -- List the number of records with the same score (print the score and the number of records with this score)
-SELECT score, COUNT(*) AS 'number'
-FROM second_table
-GROUP BY score
-ORDER BY 'number' DESC
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC
