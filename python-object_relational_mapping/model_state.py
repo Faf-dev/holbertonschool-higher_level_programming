02308b9645ec71388a50669f976ac325808535a3
Faf-dev <noaillesjulien@gmail.com>
2025-03-05 17:28:42 +0100

    fix comment for the State class

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,6 +10,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    State class : Inherits from Base
+    """
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, primary_key=True,)
     name = Column(String(128), nullable=False)
