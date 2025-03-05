bca155b8fe6cf7159b44d67911ad7b2d1cff582b
Faf-dev <noaillesjulien@gmail.com>
2025-03-05 17:27:02 +0100

    push task 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Create a class definition of a state and an instance
+base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True,)
+    name = Column(String(128), nullable=False)
