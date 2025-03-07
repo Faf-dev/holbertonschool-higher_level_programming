24865b0019f0226d4c5eb6070dc3cd9199102582
Faf-dev <noaillesjulien@gmail.com>
2025-03-07 13:32:23 +0100

    attempt t7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,8 +5,7 @@ lists all State objects from the database hbtn_0e_6_usa
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 
 if __name__ == '__main__':
 
